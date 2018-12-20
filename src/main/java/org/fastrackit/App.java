f8387812b496fbcd8239a2e2535427cb9a50cac4
calincj <acalin.andrei83@gmail.com>
2018-12-21 01:25:56 +0200

    :D

---
+++ src/main/java/org/fastrackit/App.java
@@ -0,0 +1,13 @@
+package org.fastrackit;
+
+/**
+ * RacingGameExperiments!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "RacingGameExperiments!" );
+    }
+}
